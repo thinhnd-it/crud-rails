bb31d891391e0dcfdead9c4dcbf06af960798a68
Ted Nguyen <ted.nguyen.goldenowl@gmail.com>
2022-05-06 14:02:44 +0700

    add validation for friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,10 @@
 class Friend < ApplicationRecord
     belongs_to :user
+    
+    VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+    validates :email , presence: true,uniqueness:{case_sensetive:false},
+    format:{with:VALID_EMAIL_REGEX,multiline:true}
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :phone, presence: true
 end
