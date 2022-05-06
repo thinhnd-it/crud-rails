e257d13f1a2e777a556473e7a262076201422fa9
Ted Nguyen <ted.nguyen.goldenowl@gmail.com>
2022-05-06 16:16:51 +0700

    add custom field to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_102419) do
+ActiveRecord::Schema.define(version: 2022_05_06_084331) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2022_05_05_102419) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone"
+    t.datetime "dob"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
