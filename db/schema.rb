ff4c4027999dc7b23bd448ec31478886b497faf8
Ted Nguyen <ted.nguyen.goldenowl@gmail.com>
2022-05-10 09:26:07 +0700

    initial frame

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_084331) do
+ActiveRecord::Schema.define(version: 2022_05_09_051341) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -23,16 +23,12 @@ ActiveRecord::Schema.define(version: 2022_05_06_084331) do
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
